8542ac43f59461cfb2daee73b30d7648e29986b6
halilintar8 <halilintar8@yahoo.com>
2020-03-29 05:36:28 -0700

    Update

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php //get_template_part('templates/page', 'header')234; ?>
+<?php //get_template_part('templates/page', 'header')2345; ?>
 
 <?php if (!have_posts()) : ?>
     <div class="alert alert-warning">
